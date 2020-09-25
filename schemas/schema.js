0d455a20542d264ef4d2491c315f7aa01623f438
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-09-25 15:25:31 -0300

    Add post and author schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,8 +1,67 @@
 import createSchema from 'part:@sanity/base/schema-creator'
-
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([])
+  types: schemaTypes.concat([
+    {
+      name: 'author',
+      title: 'Autor',
+      type: 'document',
+      fields: [
+        {
+          name: 'slug',
+          title: 'Slug',
+          type: 'slug'
+        },
+        {
+          name: 'profilePicture',
+          title: 'Foto de Perfil',
+          type: 'image'
+        },
+        {
+          name: 'fullName',
+          title: 'Nome Completo',
+          type: 'string'
+        },
+        {
+          name: 'bio',
+          title: 'Bio',
+          type: 'string'
+        }
+      ]
+    },
+    {
+      name: 'post',
+      title: 'Postagem',
+      type: 'document',
+      fields: [
+        {
+          name: 'slug',
+          title: 'Slug',
+          type: 'slug'
+        },
+        {
+          name: 'title',
+          title: 'Título',
+          type: 'string'
+        },
+        {
+          name: 'subtitle',
+          title: 'Subtítulo',
+          type: 'string'
+        },
+        {
+          name: 'date',
+          title: 'Data',
+          type: 'date'
+        },
+        {
+          name: 'coverImage',
+          title: 'Imagem de Capa',
+          type: 'image'
+        }
+      ]
+    }
+  ])
 })
